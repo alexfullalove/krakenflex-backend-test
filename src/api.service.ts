0ee46f1bff6c37563cf9a59f6bd4e4abcf29c4cf
alexfullalove <alexanderfullalove@gmail.com>
2022-07-22 23:07:09 +0100

    added api service

---
+++ src/api.service.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+import apiConfig from "./api.config";
+
+// get outages
+
+// get site info
+
+// post site outages
+export const logVars = () => {
+  console.log(apiConfig);
+};

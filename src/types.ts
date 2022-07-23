e911eaaec00e7e3d142097df9b68f431a2441914
alexfullalove <alexanderfullalove@gmail.com>
2022-07-23 10:06:02 +0100

    added types for site infor and devices

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,12 @@ export interface Outages {
 export interface SiteOutageData extends Outages {
   name: string;
 }
+
+export interface SiteInfo extends Devices {
+  devices: Devices[];
+}
+
+interface Devices {
+  id: string;
+  name: string;
+}

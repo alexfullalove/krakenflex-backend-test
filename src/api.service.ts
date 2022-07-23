e507b93cc93c0a35ef95e5d0ad391dd2e868c7d7
alexfullalove <alexanderfullalove@gmail.com>
2022-07-23 09:35:43 +0100

    added get outages function

--- src/api.service.ts
+++ src/api.service.ts
@@ -1,11 +1,15 @@
 import axios from "axios";
 import apiConfig from "./api.config";
+import { Outages } from "./types";
 
-// get outages
-
+export const getOutages = async (): Promise<Outages[]> => {
+  const response = await axios.get(`${apiConfig.baseUrl}/outages`, {
+    headers: {
+      "x-api-key": apiConfig.apiKey,
+    },
+  });
+  return response.data;
+};
 // get site info
 
 // post site outages
-export const logVars = () => {
-  console.log(apiConfig);
-};

526ab60c3480d327447a32bb3370688c216f4a1b
alexfullalove <alexanderfullalove@gmail.com>
2022-07-23 14:17:34 +0100

    added post request and final message

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as apiFns from "./api.service";
+import * as logicFns from "./outage-logic";
 
 const handler = async (siteId: string): Promise<void> => {
   try {
@@ -8,10 +9,19 @@ const handler = async (siteId: string): Promise<void> => {
     console.log("Getting site info ...");
     const siteInfo = await apiFns.getSiteInfo(siteId);
     console.log(`Successfully retrieved site info for ${siteInfo.name}`);
-    // filter and merge dat
+    const filteredSiteOutages = logicFns.filterAndMergeSiteOutages(
+      outages,
+      siteInfo.devices
+    );
+    console.log(
+      `Found ${filteredSiteOutages.length} for site: ${siteInfo.name}`
+    );
 
-    // await apiFns.postSiteOutages(siteId, siteOutages)
-  } catch (err) {
+    await apiFns.postSiteOutages(siteId, filteredSiteOutages);
+    console.log(
+      `Successfully posted ${filteredSiteOutages.length} outages to site-outages endpoint`
+    );
+  } catch (err: any) {
     console.log(err);
   }
 };

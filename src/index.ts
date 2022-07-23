0361c11f5806b29d97414a3aacead27ce3c801d8
alexfullalove <alexanderfullalove@gmail.com>
2022-07-23 22:41:01 +0100

    refarctored imports

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,27 @@
-import * as apiFns from "./api.service";
-import * as logicFns from "./outage-logic";
+import { getOutages, getSiteInfo, postSiteOutages } from "./api.service";
+import { filterAndMergeSiteOutages } from "./outage-logic";
 
 const siteId = "norwich-pear-tree";
 
 export const handler = async (siteId: string): Promise<void> => {
   try {
     console.log("Fetching all outages and site info ...");
-    const outages = await apiFns.getOutages();
+    const outages = await getOutages();
     if (outages.length === 0) {
       console.log("No outages were returned");
       return;
     }
 
-    const siteInfo = await apiFns.getSiteInfo(siteId);
-    const filteredSiteOutages = logicFns.filterAndMergeSiteOutages(
-      outages,
-      siteInfo.devices
-    );
+    const siteInfo = await getSiteInfo(siteId);
+    const { devices } = siteInfo;
+    const filteredSiteOutages = filterAndMergeSiteOutages(outages, devices);
 
     if (filteredSiteOutages.length === 0) {
       console.log("No outages matched any of the devices");
       return;
     }
 
-    await apiFns.postSiteOutages(siteId, filteredSiteOutages);
+    await postSiteOutages(siteId, filteredSiteOutages);
     console.log(
       `Successfully posted ${filteredSiteOutages.length} devices out of ${outages.length} outages to site-outages endpoint for site: ${siteInfo.name}`
     );

898205fedbf9d10114d6c8add8eff1d44f87517b
alexfullalove <alexanderfullalove@gmail.com>
2022-07-23 14:18:23 +0100

    added logic to filter out old messages and add device name to object

---
+++ src/outage-logic.ts
@@ -0,0 +1,18 @@
+import { Outages, SiteOutageData, Devices } from "./types";
+
+const dateCutoff = `2022-01-01T00:00:00.000Z`;
+
+export const filterAndMergeSiteOutages = (
+  outages: Outages[],
+  devices: Devices[]
+): SiteOutageData[] => {
+  let siteOutages: SiteOutageData[] = [];
+
+  outages.filter((siteOutage) => {
+    if (siteOutage.begin >= dateCutoff) {
+      const device = devices.find((device) => device.id === siteOutage.id);
+      if (device) siteOutages.push({ name: device.name, ...siteOutage });
+    }
+  });
+  return siteOutages;
+};

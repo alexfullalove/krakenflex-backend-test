3c3bf0079114adbc260c17b83110401c38b707e7
alexfullalove <alexanderfullalove@gmail.com>
2022-07-23 09:38:54 +0100

    added call to get outages

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 import * as apiFns from "./api.service";
 
-const handler = async (siteId: string): Promise<void> => {};
+const handler = async (siteId: string): Promise<void> => {
+  try {
+    console.log("Fetching all outages ...");
+    const outages = await apiFns.getOutages();
+    console.group(`returned ${outages.length} outages`);
+  } catch (err) {
+    console.log(err);
+  }
+  // get site info
+  // filter outages if they are different ID or pre 2022 and combine remainder with site info
+  // post data to site endpoint
+};
+
+handler("norwich-pear-tree");

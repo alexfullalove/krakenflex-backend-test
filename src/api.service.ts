a6007d8c12f067f9f23906d5e2971cadc7d65722
alexfullalove <alexanderfullalove@gmail.com>
2022-07-23 10:11:11 +0100

    get site info function

--- src/api.service.ts
+++ src/api.service.ts
@@ -1,9 +1,18 @@
 import axios from "axios";
 import apiConfig from "./api.config";
-import { Outages } from "./types";
+import { Outages, SiteInfo } from "./types";
 
 export const getOutages = async (): Promise<Outages[]> => {
   const response = await axios.get(`${apiConfig.baseUrl}/outages`, {
+    headers: {
+      "x-api-key": apiConfig.apiKey,
+    },
+  });
+  return response.data;
+};
+
+export const getSiteInfo = async (siteId: string): Promise<SiteInfo> => {
+  const response = await axios.get(`${apiConfig.baseUrl}/site-info/${siteId}`, {
     headers: {
       "x-api-key": apiConfig.apiKey,
     },
